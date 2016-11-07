bd5775c59a4969c165d4888db013ad0d02a204e6
root <root@ns364070.ip-91-121-183.eu>
2016-11-07 09:50:50 +0100

    fix disk usage check

--- lxc-attach.sh
+++ lxc-attach.sh
@@ -11,10 +11,10 @@ if [[ $STATUS = "STOPPED" ]]; then exit 0; fi
 case $COMMAND in
 "df")
 	/usr/bin/sudo /usr/bin/lxc-attach -n $CTID -- /usr/bin/df -P / 2>/dev/null  || \
-	/usr/bin/sudo /usr/bin/lxc-attach -n $CTID -- /bin/df -P / 2>/dev/null | tail -n 1 
+	/usr/bin/sudo /usr/bin/lxc-attach -n $CTID -- /bin/df -P / 2>/dev/null  
 ;;
 "dfi")
 	/usr/bin/sudo /usr/bin/lxc-attach -n $CTID -- /usr/bin/df -i -P / 2>/dev/null  || \
-	/usr/bin/sudo /usr/bin/lxc-attach -n $CTID -- /bin/df -i -P / 2>/dev/null | tail -n 1 
+	/usr/bin/sudo /usr/bin/lxc-attach -n $CTID -- /bin/df -i -P / 2>/dev/null 
 ;;
 esac
